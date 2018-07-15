e3218fb55045b24314d4277d5352f67c26a7f114
Damith Senanayake <dracusds123@gmail.com>
2018-07-16 09:51:13 +1000

    Adding the input parameter dialog to ease execution

--- ENVIRT.m
+++ ENVIRT.m
@@ -1,5 +1,34 @@
 
-function ENVIRT(spectrum_file, n_reads, avg_read_length, overlap_length,trim_length, M_min, M_max, p_res, p_max, L_res, L_min, L_max, n_windows, result_folder, persist_intermediates)
+%% ENVIRT() - Run this file to execute the ENVirT algorithm. 
+
+
+function ENVIRT()
+
+[filename, path] = uigetfile('*.*', 'Select Contig Spectrum File');
+spectrum_file = fullfile(path, filename);
+contig_config_prompt = {"No. of reads", "Average Read Length", "Overlap Length", "Trim Length"};
+def_contig_config = {'', '','',''};
+contig_config = inputdlg(contig_config_prompt,"Contig Spectrum Configurations", [1, 35], def_contig_config);
+
+n_reads = str2num(contig_config{1}); avg_read_length = str2num(contig_config{2}); 
+overlap_length = str2num(contig_config{3}); trim_length = str2num(contig_config{4});
+
+search_config_prompt = {'M_min', 'M_max', 'p_res', 'p_max', 'L_res', 'L_min', 'L_max', 'n_windows'};
+def_search_config = {'1', '100000', '0.01', '5', '500', '500', '300500', '29' };
+
+search_config = inputdlg(search_config_prompt, "Search Configurations", [1, 35], def_search_config);
+M_min = search_config{1};
+M_max = search_config{2};
+p_res = str2num(search_config{3});
+p_max = str2num(search_config{4});
+L_res = str2num(search_config{5});
+L_min = str2num(search_config{6});
+L_max = str2num(search_config{7});
+n_windows = str2num(search_config{8});
+
+result_folder = uigetdir("Select Folder to Store Results");
+
+persist_intermediates = questdlg("Would you like to save the intermediate calculation files?");
 
 if (~exist('spectrum_file', 'var') || isempty(spectrum_file));
     error('Error, spectrum_file cannot be null');
@@ -16,12 +45,12 @@ end
 if ~exist('M_min', 'var') || isempty(M_min); 
     min_M = 1; 
 else
-    min_M = M_min;
+    min_M = str2num(M_min);
 end
 if ~exist('M_max', 'var') || isempty(M_max);
     max_M = 100000; 
 else
-    max_M = M_max;
+    max_M = str2num(M_max);
 end
 if ~exist('p_res','var') || isempty(p_res);
     p_res = 0.01;
@@ -91,4 +120,3 @@ catch exception
     delete(p);
 end
 end
-
